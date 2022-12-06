aaaf42316f97a07a24ce26b9c21594cda9148722
fabien servant <fabien.servant@technicolor.com>
2022-12-06 14:52:26 +0100

    graph builder node

---
+++ meshroom/nodes/aliceVision/GraphBuilder.py
@@ -0,0 +1,73 @@
+__version__ = "2.0"
+
+from meshroom.core import desc
+
+
+class GraphBuilder(desc.CommandLineNode):
+    commandLine = 'aliceVision_graphBuilder {allParams}'
+    size = desc.DynamicNodeSize('input')
+
+    category = 'Sparse Reconstruction'
+
+    inputs = [
+        desc.File(
+            name='input',
+            label='SfMData',
+            description='SfMData file.',
+            value='',
+            uid=[0],
+        ),
+        desc.ListAttribute(
+            elementDesc=desc.File(
+                name="featuresFolder",
+                label="Features Folder",
+                description="",
+                value="",
+                uid=[0],
+            ),
+            name="featuresFolders",
+            label="Features Folders",
+            description="Folder(s) containing the extracted features and descriptors."
+        ),
+        desc.ListAttribute(
+            elementDesc=desc.File(
+                name="matchesFolder",
+                label="Matches Folder",
+                description="",
+                value="",
+                uid=[0],
+            ),
+            name="matchesFolders",
+            label="Matches Folders",
+            description="Folder(s) in which computed matches are stored."
+        ),
+        desc.ChoiceParam(
+            name='describerTypes',
+            label='Describer Types',
+            description='Describer types used to describe an image.',
+            value=['dspsift'],
+            values=['sift', 'sift_float', 'sift_upright', 'dspsift', 'akaze', 'akaze_liop', 'akaze_mldb', 'cctag3', 'cctag4', 'sift_ocv', 'akaze_ocv', 'tag16h5'],
+            exclusive=False,
+            uid=[0],
+            joinChar=',',
+        ),
+        desc.ChoiceParam(
+            name='verboseLevel',
+            label='Verbose Level',
+            description='Verbosity level (fatal, error, warning, info, debug, trace).',
+            value='info',
+            values=['fatal', 'error', 'warning', 'info', 'debug', 'trace'],
+            exclusive=True,
+            uid=[],
+        )
+    ]
+
+    outputs = [
+        desc.File(
+            name='output',
+            label='SfMData',
+            description='Path to the output sfmdata file',
+            value=desc.Node.internalFolder + 'sfm.abc',
+            uid=[],
+        )
+    ]
