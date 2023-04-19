06694f407edfa205b02ca399039d1d811c9a1d98
Fabien Servant <fabien.servant@technicolor.com>
2023-04-19 14:54:06 +0200

    bootstrapSfm node

---
+++ meshroom/nodes/aliceVision/bootstrapSfm.py
@@ -0,0 +1,88 @@
+__version__ = "2.0"
+
+from meshroom.core import desc
+
+
+class BootStrapSfM(desc.AVCommandLineNode):
+    commandLine = 'aliceVision_bootstrapSfM {allParams}'
+    size = desc.DynamicNodeSize('input')
+
+    category = 'Sparse Reconstruction'
+    documentation = '''
+'''
+
+    inputs = [
+        desc.File(
+            name='input',
+            label='SfMData',
+            description='SfMData file.',
+            value='',
+            uid=[0],
+        ),
+        desc.ListAttribute(
+            elementDesc=desc.File(
+                name="featuresFolder",
+                label="Features Folder",
+                description="",
+                value="",
+                uid=[0],
+            ),
+            name="featuresFolders",
+            label="Features Folders",
+            description="Folder(s) containing the extracted features and descriptors."
+        ),
+        desc.File(
+            name='tracksFilename',
+            label='Tracks file',
+            description='Tracks file.',
+            value='',
+            uid=[0],
+        ),
+        desc.ChoiceParam(
+            name='describerTypes',
+            label='Describer Types',
+            description='Describer types used to describe an image.',
+            value=['dspsift'],
+            values=['sift', 'sift_float', 'sift_upright', 'dspsift', 'akaze', 'akaze_liop', 'akaze_mldb', 'cctag3', 'cctag4', 'sift_ocv', 'akaze_ocv', 'tag16h5'],
+            exclusive=False,
+            uid=[0],
+            joinChar=',',
+        ),
+        desc.FloatParam(
+            name='minAngleInitialPair',
+            label='Min Angle Initial Pair',
+            description='Minimum angle for the initial pair.',
+            value=5.0,
+            range=(0.1, 10.0, 0.1),
+            uid=[0],
+            advanced=True,
+        ),
+        desc.FloatParam(
+            name='maxAngleInitialPair',
+            label='Max Angle Initial Pair',
+            description='Maximum angle for the initial pair.',
+            value=40.0,
+            range=(0.1, 60.0, 0.1),
+            uid=[0],
+            advanced=True,
+        ),
+        desc.ChoiceParam(
+            name='verboseLevel',
+            label='Verbose Level',
+            description='Verbosity level (fatal, error, warning, info, debug, trace).',
+            value='info',
+            values=['fatal', 'error', 'warning', 'info', 'debug', 'trace'],
+            exclusive=True,
+            uid=[],
+        )
+    ]
+
+    outputs = [
+        desc.File(
+            name='output',
+            label='SfMData',
+            description='Path to the output sfmdata file',
+            value=desc.Node.internalFolder + 'sfm.abc',
+            uid=[],
+        )
+    ]
